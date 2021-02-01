e6cf9e05b8945a89054c6b8344798dd6f90f26d0
Francesco Evangelista <francesco.evangelista@gmail.com>
2021-02-01 10:18:21 -0500

    Update chapter on molecular hamiltonian

---
+++ compile_notes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import os
+from shutil import copy
+
+chapters = ['molecular-hamiltonian']
+
+root_dir = os.getcwd()
+
+for ch in chapters:
+    print(ch)
+    chapter_dir = ch #os.path.join('Notes',ch)
+    os.chdir(chapter_dir)
+    os.system(f'xelatex {ch}.tex')
+    os.system(f'bibtex {ch}.tex')
+    os.system(f'xelatex {ch}.tex')
+    os.system(f'xelatex {ch}.tex')
+    copy(f'{ch}.pdf', f'../pdf')
+    os.chdir(root_dir)
+
+#main_dir = os.path.join('Notes','Main')
+#main = 'chem371-notes'
+#os.chdir(main_dir)
+#os.system(f'xelatex {main}.tex')
+#os.system(f'bibtex {main}.tex')
+#os.system(f'xelatex {main}.tex')
+#os.system(f'xelatex {main}.tex')
+#copy(f'{main}.pdf', f'../../pdfs')
+#os.chdir(root_dir)
